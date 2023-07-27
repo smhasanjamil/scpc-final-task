5e7288a6aefa0a3ce7de5be52ba925bfc973fa50
smhasanjamil <smhasanjamil23@gmail.com>
2023-07-27 18:18:33 +0600

    Task 4

---
+++ Task_4.js
@@ -0,0 +1,29 @@
+// Task 4 
+// Create a function that takes a sorted array of numbers and a target value as input. The
+// function should find two numbers in the array that add up to the target value. Return an array
+// containing the indices of the two numbers.
+// Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2:
+// 3 + 6 = 9)
+
+const findPairIndices = (arr, target) => {
+    let i = 0;
+    let j = arr.length - 1;
+
+    while (i < j) {
+        const sum = arr[i] + arr[j];
+        if (sum === target) {
+            return [i, j];
+        } else if (sum < target) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+
+    return null;
+};
+
+const input = [1, 3, 6, 8, 11, 15];
+const target = 17;
+const output = findPairIndices(input, target);
+console.log(output);
